a0e4db2cd0057d5804dd1bf2139a37b50335cd5d
JKamlah <jan.kamlah@gmail.com>
2021-10-01 15:33:04 +0200

    Update setup to install replace_lines function.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ Installs:
     - ocrd-segment-from-coco
     - ocrd-segment-extract-pages
     - ocrd-segment-extract-regions
+    - ocrd-segment-replace-lines
     - ocrd-segment-extract-lines
     - ocrd-segment-extract-words
     - ocrd-segment-extract-glyphs
@@ -28,8 +29,8 @@ setup(
     description='Page segmentation and segmentation evaluation',
     long_description=codecs.open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    author='Konstantin Baierer, Kay-Michael Würzner, Robert Sachunsky',
-    author_email='unixprog@gmail.com, wuerzner@gmail.com, sachunsky@informatik.uni-leipzig.de',
+    author='Konstantin Baierer, Jan Kamlah, Kay-Michael Würzner, Robert Sachunsky',
+    author_email='unixprog@gmail.com, jan.kamlah@gmail.com, wuerzner@gmail.com, sachunsky@informatik.uni-leipzig.de',
     url='https://github.com/OCR-D/ocrd_segment',
     license='Apache License 2.0',
     packages=find_packages(exclude=('tests', 'docs')),
@@ -44,6 +45,7 @@ setup(
             'ocrd-segment-from-coco=ocrd_segment.cli:ocrd_segment_from_coco',
             'ocrd-segment-extract-pages=ocrd_segment.cli:ocrd_segment_extract_pages',
             'ocrd-segment-extract-regions=ocrd_segment.cli:ocrd_segment_extract_regions',
+            'ocrd-segment-replace-lines=ocrd_segment.cli:ocrd_segment_replace_lines',
             'ocrd-segment-extract-lines=ocrd_segment.cli:ocrd_segment_extract_lines',
             'ocrd-segment-extract-words=ocrd_segment.cli:ocrd_segment_extract_words',
             'ocrd-segment-extract-glyphs=ocrd_segment.cli:ocrd_segment_extract_glyphs',
